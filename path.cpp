0fed5f3c3f33a052683df2dc7b61a1a97dd902ae
U-SAP_ALL\I303035 <I303035@SZXN50832849A.sap.corp>
2013-10-26 15:28:25 +0800

    .

--- path.cpp
+++ path.cpp
@@ -1,85 +1,69 @@
 //试图求一条极大路径
 //从u出发
 //next[]记录了这条路径每个点下一个点
+#include <assert.h>
 #include <iostream>
 #include <set>
 using namespace std;
 #define rep(i,m,n) for(int i=m;i<=n;i++)
 #define N 100
 bool vis[N];
-int next[N];
+int next[N];//初始为-1
 set<int> Edges[N];
-//从u向后扩展
-void extend(int u){
+//从u向某个方向扩展
+//@return 扩展到的最后一个点
+int extend(int u,bool direction){
   while(true){
     vis[u]=true;
-    int s=u;
-    next[u]=-1;
-    for(set<int>::iterator it=Edges[u].begin();
-        it!=Edges[u].end();it++){
+    int tmp=u;
+    for(set<int>::iterator it=Edges[u].begin(); it!=Edges[u].end();it++){
       int v=*it;
       if(!vis[v]){ 
-        next[u]=v;
+        if(direction) next[u]=v;
+        else next[v]=u;
         u=v;
         break;
       }
     }
-    if(s==u) break;
-  }
-}
-//从u向前扩展 
-int rextend(int u){
-  while(true){
-    vis[u]=true;
-    int s=u;
-    for(set<int>::iterator it=Edges[u].begin();
-        it!=Edges[u].end();it++){
-      int v=*it;
-      if(!vis[v]){ 
-        next[v]=u;
-        u=v;
-        break;
-      }
-    }
-    if(s==u) break;
+    if(tmp==u) break;
   }
   return u;
-} 
-//u!=t
-//寻找(u,t）之间的路径
-bool dfs(int u,int t){
-  if(u==t) return true;
+}
+//寻找(s,t）之间的路径,不包含st
+//u为当前访问的点
+//@param s,t都是已标记的
+bool dfs(int s,int u,int t){
+  //bake vis[u]
+  bool tmp=vis[u];
+  vis[u]=true;
   for(set<int>::iterator it=Edges[u].begin();
       it!=Edges[u].end();it++){
     int v=*it;
-    if(v==t||(!vis[v]&&dfs(v,t))){ 
-      vis[v]=true;
+    if((s!=u&&v==t)||!vis[v]&&dfs(s,v,t)){ 
       next[u]=v;
       return true;
     }
   }
+  //recover vis[u]
+  vis[u]=tmp;
   return false;
 } 
-int getPath(int u){
-  extend(u);
-  int t=u=rextend(u);
-  int v=u;
+int extendPath(int u){
+  int t=u,v=u;
   while(true){
     u=v;
-    v=next[u];
-    if(v==-1) break;
+    if(next[u]==-1) break;
     while(true){
       v=next[u];
-      dfs(u,v);
-      if(v==next[u]) break; 
+      assert(v!=-1);
+      if(!dfs(u,u,v)) break;
     }
   }
   return t;
 }
 int main(){
   rep(i,1,N) next[i]=-1,vis[i]=false;
-  int m;
-  cin>>m;
+  int m; cin>>m;
   for(int i=1;i<=m;i++){
     int a,b;
     cin>>a>>b;
@@ -87,7 +71,17 @@ int main(){
     Edges[a].insert(b);
     Edges[b].insert(a);
   }
-  int u=getPath(1);
+
+  int u=1;
+  extend(u,true);
+  int tmp=u=extend(u,false);
+  while(u!=-1){
+    cout<<u<<" ";
+    u=next[u]; 
+  }
+  cout<<endl<<"after extended"<<endl;
+  u=tmp;
+  extendPath(u);
   while(u!=-1){
     cout<<u<<" ";
     u=next[u]; 
